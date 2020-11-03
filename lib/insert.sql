3e81374dfae6276b0edeb2c844f3cb4c765fdebc
ConnorMul <mulhollandconnor@icloud.com>
2020-11-03 18:58:22 -0500

    commit tree e94ca5d7144b0b29eb6493b13f18d3522b3caf5a

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 25, "Male", "Black", "Kooky", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 45, "Female", "Brown", "Rowdy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 23, "Female", "Black", "Kooky", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 58, "Male", "Brown", "Stern", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 32, "Female", "White", "Mean", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 54, "Male", "Green", "Shady", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 25, "Female", "Black", "Sweet", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 30, "Male", "Black", "Evil", 1);
